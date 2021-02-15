42c589f64a5c08bde556176cfadf6a41451dd55b
tgoyu215 <bach5353op@gmail.com>
2021-02-15 09:16:43 +0900

    footer(プラポリと利用規約)追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
-  get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
+  get  '/about',            to: 'static_pages#about'
+  get  '/contact',          to: 'static_pages#contact'
+  get  '/privacy_policy',   to: 'static_pages#privacy_policy'
+  get  '/terms_of_service', to: 'static_pages#terms_of_service'
 end
