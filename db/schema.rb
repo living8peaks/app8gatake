5ea398692162b71cc0a150b56baf33a6448dd578
tgoyu215 <bach5353op@gmail.com>
2021-02-17 23:43:29 +0900

    ユーザー検証のためのバリデーション実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_131154) do
+ActiveRecord::Schema.define(version: 2021_02_17_144044) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "nonscreen_last_name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_02_17_131154) do
     t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
