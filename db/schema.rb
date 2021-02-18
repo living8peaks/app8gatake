303a393d9815872eeb8afa6b0feb4dcd43d2d96e
tgoyu215 <bach5353op@gmail.com>
2021-02-18 20:34:01 +0900

    パスワードのハッシュ化

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_144044) do
+ActiveRecord::Schema.define(version: 2021_02_18_102922) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "nonscreen_last_name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_02_17_144044) do
     t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
