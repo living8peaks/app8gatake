88e42c85833939dad34f9aae40a0d0b9f13d4624
living8peaks <bach5353op@gmail.com>
2021-02-19 22:04:05 +0900

    resources :users 追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get  '/privacy_policy',   to: 'static_pages#privacy_policy'
   get  '/terms_of_service', to: 'static_pages#terms_of_service'
   get  '/signup',           to: 'users#new'
+  resources :users
 end
