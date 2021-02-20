b32a5f6bb63e4acfc3647868573cc3a8ce7d2819
living8peaks <bach5353op@gmail.com>
2021-02-20 22:27:51 +0900

    ユーザー登録ページ作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,5 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)(?=.*?\W)[!-~]+\z/i
   validates :password, presence: true, length: { minimum: 8, maximum: 16 },
                         format: { with: VALID_PASSWORD_REGEX }
+  enum gender_identities: { 男性:0, 女性:1, その他:2, 回答しない:3 }
 end
