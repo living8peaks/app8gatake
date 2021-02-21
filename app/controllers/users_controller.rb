9fc9227e16d221eaea3b5938c620c05118286489
living8peaks <bach5353op@gmail.com>
2021-02-21 10:47:58 +0900

    ユーザー登録フォームにstrong_parameters導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,28 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(
+        :nonscreen_last_name,
+        :nonscreen_first_name,
+        :name,
+        :email,
+        :password,
+        :password_confirmation,
+        :gender_identities,
+        :self_info,
+      )
+    end
 end
