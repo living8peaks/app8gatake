c7da3bbf6b522ff0a1d9f53ae92ee1044b04946d
living8peaks <bach5353op@gmail.com>
2021-02-22 19:51:38 +0900

    flash導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "#{@user.name}さんのアカウントを作成しました"
       redirect_to user_path(@user)
     else
       render 'new'
