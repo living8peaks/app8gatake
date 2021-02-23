0f939904494af3af9dfdce4231f4da6ac1b6ae3d
living8peaks <bach5353op@gmail.com>
2021-02-23 21:32:24 +0900

    sessionsリソースに名前付きルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   root 'static_pages#top'
   get  '/about',            to: 'static_pages#about'
   get  '/contact',          to: 'static_pages#contact'
   get  '/privacy_policy',   to: 'static_pages#privacy_policy'
   get  '/terms_of_service', to: 'static_pages#terms_of_service'
   get  '/signup',           to: 'users#new'
+
   resources :users
+
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
