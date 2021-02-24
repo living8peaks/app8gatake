c88b447d24481a4224a1e58893b63448c662de33
living8peaks <bach5353op@gmail.com>
2021-02-24 22:12:53 +0900

    ログアウトできるように記憶ダイジェストをnilで更新

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,7 +12,7 @@ module SessionsHelper
 
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: [:user_id])
+      @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
       if user&.authenticated?(cookies[:remember_token])
@@ -24,5 +24,17 @@ module SessionsHelper
 
   def logged_in?
     !!current_user
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
