7daf1e3e68e67f126846be3e716509b05bfb20a7
living8peaks <bach5353op@gmail.com>
2021-02-24 21:53:07 +0900

    セッションを永続的にするためのヘルパーメソッドの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   before_save { self.email = email.downcase }
   validates :nonscreen_last_name, presence: true, length: { maximum: 20 }
   validates :nonscreen_first_name, presence: true, length: { maximum: 20 }
-  validates :name, presence: true, length: { maximum: 20 }, 
+  validates :name, presence: true, length: { maximum: 20 },
                    uniqueness: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -26,5 +26,9 @@ class User < ApplicationRecord
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 end
