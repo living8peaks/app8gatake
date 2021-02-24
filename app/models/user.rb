d1ef1099b6c6307840b903dce08c6cdd4f4386e3
living8peaks <bach5353op@gmail.com>
2021-02-24 20:52:09 +0900

    記憶トークンと暗号化

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+  before_save { self.email = email.downcase }
   validates :nonscreen_last_name, presence: true, length: { maximum: 20 }
   validates :nonscreen_first_name, presence: true, length: { maximum: 20 }
   validates :name, presence: true, length: { maximum: 20 }, 
@@ -11,4 +13,18 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)(?=.*?\W)[!-~]+\z/i
   validates :password, presence: true, length: { minimum: 8, maximum: 16 }, allow_nil: true, format: { with: VALID_PASSWORD_REGEX }
   enum gender_identities: { 男性: 0, 女性: 1, その他: 2, 回答しない: 3 }
+
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
 end
