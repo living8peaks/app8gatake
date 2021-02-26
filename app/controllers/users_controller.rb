b4703f91c70e83fb3962896e66b5d4be3353527f
living8peaks <bach5353op@gmail.com>
2021-02-26 20:30:21 +0900

    フレンドリーフォワーディング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,6 +55,7 @@ class UsersController < ApplicationController
     # ログインしていないユーザーが保護されたページにアクセスできないようにするため
     def login_required
       unless logged_in?
+        store_location
         flash[:danger] = 'ログインしてください'
         redirect_to login_url
       end
