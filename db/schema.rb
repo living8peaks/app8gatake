53826edb1993a4022d925232e27f2895f04346ad
living8peaks <bach5353op@gmail.com>
2021-02-27 11:22:23 +0900

    usersテーブルにadminカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_27_002148) do
+ActiveRecord::Schema.define(version: 2021_02_27_021959) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "nonscreen_last_name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_02_27_002148) do
     t.string "password_digest"
     t.string "remember_digest"
     t.string "image_name"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
