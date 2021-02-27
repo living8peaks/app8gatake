f459644ebc09ce5b5909ddbf60e87167265f0021
living8peaks <bach5353op@gmail.com>
2021-02-27 09:27:46 +0900

    usersテーブルにimage_name(プロフィール画像用)カラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_112129) do
+ActiveRecord::Schema.define(version: 2021_02_27_002148) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "nonscreen_last_name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_02_24_112129) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.string "image_name"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
