c5c028125877ba487e315adc3519e7cd4211e7ac
living8peaks <bach5353op@gmail.com>
2021-02-28 10:40:49 +0900

    サンプルユーザーの有効化

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,15 +2,20 @@ User.create!(
   name: 'tarou227',
   email: 'example@g.com',
   password: 'asdf1234!',
-  password_confirmation: 'asdf1234!'
+  password_confirmation: 'asdf1234!',
+  admin: true,
+  activated: true,
+  activated_at: Time.zone.now
 )
-99.times do |n|
+60.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@g.com"
   User.create!(
     name: name,
     email: email,
     password: 'asdf1234!',
-    password_confirmation: 'asdf1234!'
+    password_confirmation: 'asdf1234!',
+    activated: true,
+    activated_at: Time.zone.now
   )
 end
