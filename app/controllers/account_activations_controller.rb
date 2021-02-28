0bc5b12f3941778a555ec9bfe31d258ee42c1ce2
living8peaks <bach5353op@gmail.com>
2021-02-28 12:53:03 +0900

    アカウント有効化のためのeditアクション実装

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,15 @@
 class AccountActivationsController < ApplicationController
+
+  def edit
+    user = User.find_by(email: params[:email])
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.activate
+      log_in user
+      flash[:success] = 'アカウントの作成が完了しました！'
+      redirect_to user
+    else
+      flash[danger] ='アカウントの有効化ができませんでした'
+      redirect_to root_path
+    end
+  end
 end
