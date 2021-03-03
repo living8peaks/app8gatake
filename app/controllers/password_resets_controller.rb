5385373928426105ea3252ef11cef5b47088fcaa
living8peaks <bach5353op@gmail.com>
2021-03-03 22:00:46 +0900

    editアクションで再設定

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,7 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user,   only: %i[edit update]
+  before_action :valid_user, only: %i[edit update]
+
   def new
   end
 
@@ -17,4 +20,16 @@ class PasswordResetsController < ApplicationController
 
   def edit
   end
+
+  private
+
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    def valid_user
+      unless(@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+        redirect_to root_path
+      end
+    end
 end
