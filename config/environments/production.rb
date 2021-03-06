144a00d663af22ac520b2cd12123d1086e221612
living8peaks <bach5353op@gmail.com>
2021-03-06 20:09:20 +0900

    Revert "Revert "user_mailer.rb及びproduction.rb変更""
    This reverts commit 0a25790c8fbbfda62e2241173d38e9af175cc196.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  #config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -63,17 +63,18 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
+   config.action_mailer.raise_delivery_errors = true
    config.action_mailer.delivery_method = :smtp
    host = 'https://secret-wildwood-73223.herokuapp.com/'
    config.action_mailer.default_url_options = { host: host }
-   ActionMailer::Base.smtp_settings = {
+   config.action_mailer.smtp_settings = {
     address:"smtp.gmail.com",
     domain: 'gmail.com',
     port:587,
-    user_name: Rails.application.credentials.gmail[:user_name],
-    password: Rails.application.credentials.gmail[:password],
+    user_name: ENV["GMAIL_USER_NAME"],
+    password: ENV["GMAIL_PASSWORD"],
     authentication: :plain,
+    enable_starttls_auto: true
   }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
