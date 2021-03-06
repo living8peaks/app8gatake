0e4949e4818779285f1a446581d5cfd640cabd97
living8peaks <bach5353op@gmail.com>
2021-03-06 14:14:13 +0900

    production.rb(syntaxError修正)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
     user_name: Rails.application.credentials.gmail[:user_name],
     password: Rails.application.credentials.gmail[:password],
     authentication: :plain,
-  # }
+  }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
