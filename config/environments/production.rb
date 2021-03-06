b28623bc1ab5da525ab75a45c613efa533e5be6f
living8peaks <bach5353op@gmail.com>
2021-03-06 21:40:10 +0900

    production.rb(host追加)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,19 @@ Rails.application.configure do
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
   }
+  config.action_mailer.raise_delivery_errors = true
+  host = 'https://secret-wildwood-73223.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:'smtp.sendgrid.net',
+    domain: 'herokuapp.com',
+    port:587,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
