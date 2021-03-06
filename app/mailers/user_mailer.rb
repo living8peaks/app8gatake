4179a44bd67a3f74c51617a3b674e14260d9dd1c
living8peaks <bach5353op@gmail.com>
2021-03-06 17:16:55 +0900

    user_mailer.rb及びproduction.rb変更

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,11 +2,11 @@ class UserMailer < ApplicationMailer
 
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: 'アカウントの有効化'
+    mail to: @user.email, subject: 'アカウントの有効化'
   end
 
   def password_reset(user)
     @user = user
-    mail to: user.email, subject: 'パスワードの再設定'
+    mail to: @user.email, subject: 'パスワードの再設定'
   end
 end
