14faaca2ad75c497b16cdfc3bf36ea36fb823aca
living8peaks <bach5353op@gmail.com>
2021-03-07 10:29:27 +0900

    production.rb smtp_settings変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,11 +80,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:'smtp.sendgrid.net',
-    domain: 'heroku.com',
+    address:"smtp.gmail.com",
+    domain: 'gmail.com',
     port:587,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
+    user_name: ENV["GMAIL_USER_NAME"],
+    password: ENV["GMAIL_PASSWORD"],
     authentication: :plain,
     enable_starttls_auto: true
   }
