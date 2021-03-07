5a43d1c463e088472c6bcbb34ca5b590e941d9ca
living8peaks <bach5353op@gmail.com>
2021-03-07 11:10:08 +0900

    config.action_mailer.default_url_options変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   host = 'https://guarded-hamlet-93884.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:"smtp.gmail.com",
