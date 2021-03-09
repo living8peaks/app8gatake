69f894ec3753e7c80cad03494483bf90e8ded75a
living8peaks <bach5353op@gmail.com>
2021-03-09 19:28:41 +0900

    Postコントローラにbeforeフィルター(login_required)実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+
+  private
+
+    # ログインしていないユーザーが保護されたページにアクセスできないようにするため
+    def login_required
+      unless logged_in?
+        store_location
+        flash[:danger] = 'ログインしてください'
+        redirect_to login_url
+      end
+    end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,11 @@
 class PostsController < ApplicationController
+  before_action :login_required, only: %i[create, destroy]
+
+  def create
+  
+  end
+
+  def destroy
+  
+  end
 end
