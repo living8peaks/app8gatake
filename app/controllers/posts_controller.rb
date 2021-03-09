b7b51065e757e9cb9c62c6f40f76100159e1e4d0
living8peaks <bach5353op@gmail.com>
2021-03-09 18:29:03 +0900

    User詳細ページにposts表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,2 @@
 class PostsController < ApplicationController
-
-  def index
-    @posts = Post.page(params[:page]).per(20)
-  end
-
-  def show
-  
-  end
-
-  def new
-  end
-
-  def create
-    @post = Post.new(post_params)
-    if @post.save
-      flash[:success] = 'Post created!'
-      redirect_to 'root_path'
-    else
-      render 'posts_path'
-    end
-  end
-
-  private
-
-    def post_params
-      params.require(:post).permit(:content)
-    end
 end
