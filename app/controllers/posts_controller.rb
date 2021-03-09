a0299366688320a461eeccda66c875723834a62e
living8peaks <bach5353op@gmail.com>
2021-03-09 19:36:04 +0900

    createアクション(Post)及びストロングパラメーター実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,22 @@ class PostsController < ApplicationController
   before_action :login_required, only: %i[create, destroy]
 
   def create
-  
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = '投稿を作成しました！'
+      redirect_to @user
+    else
+      render 'posts_path'
+    end
   end
 
   def destroy
   
   end
+
+  private
+
+    def post_params
+      params.requir(:post).permit(:content)
+    end
 end
