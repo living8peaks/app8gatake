07de457411712cb866c311d8e965434ff8c8588b
living8peaks <bach5353op@gmail.com>
2021-03-09 20:24:45 +0900

    postの投稿機能及びフォーム作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,16 @@
 class PostsController < ApplicationController
-  before_action :login_required, only: %i[create, destroy]
+  before_action :login_required, only: %i[index create, destroy]
+
+  def index
+    @post = current_user.posts.build if logged_in?
+    @posts = Post.page(params[:page]).per(20)
+  end
 
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
       flash[:success] = '投稿を作成しました！'
-      redirect_to @user
+      redirect_to user_path(current_user)
     else
       render 'posts_path'
     end
@@ -18,6 +23,6 @@ class PostsController < ApplicationController
   private
 
     def post_params
-      params.requir(:post).permit(:content)
+      params.require(:post).permit(:content)
     end
 end
