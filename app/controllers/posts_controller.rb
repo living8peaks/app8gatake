e97823be1dd76103799b8f1aa8313a4b2476dbcb
living8peaks <bach5353op@gmail.com>
2021-03-10 22:20:33 +0900

    posts/show骨格作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,14 @@
 class PostsController < ApplicationController
-  before_action :login_required, only: %i[index create destroy]
+  before_action :login_required, only: %i[index show create destroy]
   before_action :correct_user, only: :destroy
 
   def index
     @post = current_user.posts.build
     @feed_items = current_user.feed.page(params[:page]).per(10)
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
   def create
