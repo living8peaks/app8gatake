e1cdc92be5aaea888644818e32be8dec9ccf4367
living8peaks <bach5353op@gmail.com>
2021-03-12 11:43:06 +0900

    avatarバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,7 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)(?=.*?\W)[!-~]+\z/i
   validates :password, presence: true, length: { minimum: 8, maximum: 16 }, allow_nil: true, format: { with: VALID_PASSWORD_REGEX }
   enum gender_identities: { 男性: 0, 女性: 1, その他: 2, 回答しない: 3 }
+  validates :image, content_type: { in: %w[avatar/jpeg avatar/gif avatar/png], message: '有効な画像形式にしてください' }, size: { less_than: 5.megabytes, message: '5MB未満の画像にしてください' }
 
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MINdependent_COST : BCrypt::Engine.cost
