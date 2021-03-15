04394650ab078911fd9306854cf9d8e7b9b0a504
living8peaks <bach5353op@gmail.com>
2021-03-15 19:50:09 +0900

    正方形になるようにvariant訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -83,7 +83,11 @@ class User < ApplicationRecord
   end
 
   def display_avatar
-    avatar.variant(resize_to_limit: [100, 100])
+    avatar.variant(resize_to_fill: [100, 100])
+  end
+
+  def mypage_avatar
+    avatar.variant(resize_to_fill: [60, 60])
   end
 
   def follow(other_user)
