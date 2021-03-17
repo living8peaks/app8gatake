d50df1d3cd342dfda841e926952eff0469d6e7eb
living8peaks <bach5353op@gmail.com>
2021-03-17 17:58:59 +0900

    非同期コメント機能挙動OK

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  validates :remark, presence: true
+  has_one_attached :remark_image
+  validates :remark, presence: true, length: { maximum: 80 }
   default_scope -> { order(created_at: :desc) }
+  validates :remark_image, content_type: { in: %w[image/jpeg image/gif image/png], message: '有効な画像形式にしてください' }, size: { less_than: 5.megabytes, message: '5MB未満の画像にしてください' }
+
+  def comment_image
+    remark_image.variant(resize_to_fill: [200, 200])
+  end
 end
