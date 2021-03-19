83af6945b9eeef7b3a9fa7d1e4d67b60b5fb4c0e
living8peaks <bach5353op@gmail.com>
2021-03-19 13:28:21 +0900

    コントローラにアクション追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,28 @@
 class LikesController < ApplicationController
+  before_action :login_required
+  before_action :set_post
+
+  def create
+    @like = current_user.like.build(like_params)
+    if @like.save
+      respond_to :js
+    end
+  end
+
+  def destroy
+    @like = Like.find_by(id: params[id])
+    if @like.destroy
+      respond_to :js
+    end
+  end
+
+  private
+
+    def like_params
+      params.permit(:post_id)
+    end
+
+    def set_post
+      @post = @like.post
+    end
 end
