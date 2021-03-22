cf20f8956f6a8aafda49f5b2dc681e146ef917d1
living8peaks <bach5353op@gmail.com>
2021-03-22 10:08:28 +0900

    The chat function finally confirms the behavior

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,13 +3,21 @@ module ApplicationCable
     identified_by :current_user
 
     def connect
-      reject_unauthorized_connection unless find_verified_user
+      self.current_user = find_verified_user
     end
 
-    private
+    protected
 
-      def find_verified_user
-        self.current_user = env['warden'].user
+    def find_verified_user
+      if verified_user = User.find_by(id: session['user_id'])
+        verified_user
+      else
+        reject_unauthorized_connection
       end
+    end
+
+    def session
+      cookies.encrypted[Rails.application.config.session_options[:key]]
+    end
   end
 end
