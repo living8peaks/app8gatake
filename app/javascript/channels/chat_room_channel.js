50ed0fbfb8b3f5a36e71f51986c5ad67c6ee7947
living8peaks <bach5353op@gmail.com>
2021-03-25 07:24:21 +0900

    Change chat transmission to ctrl + enter

--- app/javascript/channels/chat_room_channel.js
+++ app/javascript/channels/chat_room_channel.js
@@ -19,13 +19,15 @@ const appChatRoom = consumer.subscriptions.create("ChatRoomChannel", {
   }
 });
 
-if(/chat_rooms/.test(location.pathname)) {
-  $(document).on("keydown", ".chat-room__message-form_textarea", function(e) {
-    if (e.key === "Enter") {
-      const chat_room_id = $('textarea').data('chat_room_id')
-      appChatRoom.speak(e.target.value, chat_room_id);
-      e.target.value = '';
-      e.preventDefault();
+if (/chat_rooms/.test(location.pathname)) {
+  $(document).on('keydown', '.chat-room__message-form_textarea', function(e) {
+    if (e.ctrlKey) {
+      if (e.key === 'Enter') {
+        const chat_room_id = $('textarea').data('chat_room_id')
+        appChatRoom.speak(e.target.value, chat_room_id)
+        e.target.value = ''
+        e.preventDefault()
+      }
     }
   })
 }
