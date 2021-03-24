ccf44510abb104392b1e87bb068ed954435e4236
living8peaks <bach5353op@gmail.com>
2021-03-25 07:25:49 +0900

    Notification function behavior check OK

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,8 +1,12 @@
 class NotificationsController < ApplicationController
+  before_action :login_required
+
   def index
-    @notifications = current_user.passive_notifications.page(params[:page].per(10))
+    # @notification = Notification.find(params[:id])
+    @notifications = current_user.passive_notifications.page(params[:page]).per(10)
     @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
     end
+    #@ = @chat_room.chat_room_users.where.not(user_id: current_user.id).first.user
   end
 end
