87ee9d07ea4b77081b6f6209180797d812007b7c
living8peaks <bach5353op@gmail.com>
2021-03-28 20:59:25 +0900

    Lend_farmlands new action and strong parameter implementation

--- app/controllers/lend_farmlands_controller.rb
+++ app/controllers/lend_farmlands_controller.rb
@@ -6,8 +6,36 @@ class LendFarmlandsController < ApplicationController
   end
 
   def new
+    @lend_farnland = current_user.lend_farnlands.build(lend_farnland_params)
+  end
+
+  def create
+    
   end
 
   def edit
   end
+
+  private
+
+    def lend_farnland_params
+      params.require(:lend_farmland).permit(
+        :lend_philosophy,
+        :lend_municipality,
+        :lend_chino,
+        :lend_hara,
+        :lend_fujimi,
+        :lend_area,
+        :lend_field_type,
+        :immediate_lending,
+        :status,
+        :watering,
+        :sunlight,
+        :agricultural_machines,
+        :crops,
+        :field_situation,
+        :lending_period,
+        :lending_terms,
+      )
+    end
 end
