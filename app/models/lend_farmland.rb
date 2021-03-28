cd1d83518c078e1c659b14742ec99dbb4f65172b
living8peaks <bach5353op@gmail.com>
2021-03-28 15:41:51 +0900

    Removed lend_place column in Lend Farmlands

--- app/models/lend_farmland.rb
+++ app/models/lend_farmland.rb
@@ -1,3 +1,7 @@
 class LendFarmland < ApplicationRecord
   belongs_to :user
+  validates :lend_philosophy, length: { maximum: 1000 }
+  validates :lend_place, presence: true, length: { maximum: 100 }
+  validates :lend_other_terms, length: { maximum: 500 }
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_28_045940) do
+ActiveRecord::Schema.define(version: 2021_03_28_064010) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -68,7 +68,6 @@ ActiveRecord::Schema.define(version: 2021_03_28_045940) do
   create_table "lend_farmlands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.text "lend_philosophy"
-    t.string "lend_place"
     t.integer "lend_area"
     t.integer "lend_field_type"
     t.integer "immediate_lending"
