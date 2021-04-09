b537c25babbc9b14f327da14bb7222904355d2b8
living8peaks <bach5353op@gmail.com>
2021-04-09 21:49:54 +0900

    Create view of index action for rent_farmlands

--- app/models/rent_farmland.rb
+++ app/models/rent_farmland.rb
@@ -6,7 +6,7 @@ class RentFarmland < ApplicationRecord
   validates :user_id, presence: true
   validates :rent_philosophy, length: { maximum: 1000 }
   validates :rent_other_term, length: { maximum: 500 }
-  enum rent_municipality: { 茅野市: 0, 諏訪郡原村: 1, 諏訪郡富士見町: 2 }, _prefix: true
+  enum rent_municipality: { 茅野市: 0, 諏訪郡原村: 1, 諏訪郡富士見町: 2, 田畑の場所を限定しない: 3 }, _prefix: true
   enum rent_area: { 五畝未満: 0, 五畝〜一反: 1, 一反〜一反五畝: 2, 一反五畝〜二反: 3, 二反〜二反五畝: 4, 二反五畝以上: 5 }, _prefix: true
   enum rent_field_type: { 田: 0, 畑: 1, その他: 2 }, _prefix: true
   enum borrow_immediately: { すぐに借りたい: 0, すぐではなくて良いけど借りたい: 1 }
