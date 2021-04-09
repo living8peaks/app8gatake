296fc2c9b1e94f0a7a8da69c4714a7157c4e5ded
living8peaks <bach5353op@gmail.com>
2021-04-09 21:12:48 +0900

    Create view of edit action

--- app/models/rent_farmland.rb
+++ app/models/rent_farmland.rb
@@ -11,9 +11,9 @@ class RentFarmland < ApplicationRecord
   enum rent_field_type: { 田: 0, 畑: 1, その他: 2 }, _prefix: true
   enum borrow_immediately: { すぐに借りたい: 0, すぐではなくて良いけど借りたい: 1 }
   enum rent_desired_year: { 一年未満: 0, 一年〜二年: 1, 二年〜三年: 2, 三年以上: 3, 現時点では未定: 4 }, _prefix: true
-  enum rending_term: { 無償を希望: 0, 条件付き無償を希望: 1, 有償で構わない: 2 }
+  enum renting_term: { 無償を希望: 0, 条件付き無償を希望: 1, 有償で構わない: 2 }
   validates :rent_municipality,
-            inclusion: { in: Rentfarmland.rent_municipalities.keys }
+            inclusion: { in: RentFarmland.rent_municipalities.keys }
   validates :rent_area,
             inclusion: { in: RentFarmland.rent_areas.keys }
   validates :rent_field_type,
