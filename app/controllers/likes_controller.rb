8bb401d9fcaee3da152e02331ac58723ee3d0fc2
living8peaks <bach5353op@gmail.com>
2021-04-11 17:48:25 +0900

    Definition for lend_farmland in likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,8 +4,10 @@ class LikesController < ApplicationController
   def create
     @like = current_user.likes.build(like_params)
     @post = @like.post
+    @lend_farmland = @like.lend_farmland
     if @like.save
       post = Post.find(params[:post_id])
+      @lend_farmland = LendFarmland.find(params[:lend_farmland_id])
       post.create_notification_like(current_user)
       respond_to :js
     end
@@ -14,6 +16,7 @@ class LikesController < ApplicationController
   def destroy
   @like = Like.find_by(id: params[:id])
   @post = @like.post
+  @lend_farmland = @like.lend_farmland
     if @like.destroy
       respond_to :js
     end
