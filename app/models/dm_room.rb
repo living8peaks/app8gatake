6bc5e287d1f13123e0ccd9fbc3243fd2246fd202
living8peaks <bach5353op@gmail.com>
2021-04-25 14:14:22 +0900

    Association settings for farmland DM exchanges

--- app/models/dm_room.rb
+++ app/models/dm_room.rb
@@ -1,2 +1,4 @@
 class DmRoom < ApplicationRecord
+  has_many :dm_messages, dependent: :destroy
+  has_many :dm_entries,  dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,8 @@ class User < ApplicationRecord
   has_many :lend_farmlands, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :rent_farmlands, dependent: :destroy
+  has_many :dm_messages, dependent: :destroy
+  has_many :dm_entries, dependent: :destroy
   has_one_attached :avatar
   attr_accessor :remember_token, :activation_token, :reset_token
 
