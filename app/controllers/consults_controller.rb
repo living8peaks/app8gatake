affcca89297d5cc5f85da91f5cb95979d8d9aa3a
living8peaks <bach5353op@gmail.com>
2021-04-27 08:08:40 +0900

    consults controller logic thinking

--- app/controllers/consults_controller.rb
+++ app/controllers/consults_controller.rb
@@ -1,4 +1,7 @@
 class ConsultsController < ApplicationController
+  before_action :login_required
+
   def index
+    @consults = current_user && LendFarmland.where.not(user_id: current_user.id).where("id = ?", "params[id]")
   end
 end
--- app/controllers/matching_controller.rb
+++ app/controllers/matching_controller.rb
@@ -2,7 +2,7 @@ class MatchingController < ApplicationController
   before_action :login_required
 
   def index
-    @user = User.find(current_user.id)
+    #@user = User.find(current_user.id)
     @match_users = current_user.following && current_user.followers if logged_in?
   end
 end
