17ad07e2ac34c34960922c13ed6d2e339b10b98b
living8peaks <bach5353op@gmail.com>
2021-04-29 08:53:38 +0900

    Setting the after_destroy callback for the dm_membership model

--- app/models/dm_menmbership.rb
+++ app/models/dm_menmbership.rb
@@ -1,5 +1,13 @@
 class DmMenmbership < ApplicationRecord
+  after_destroy :destroy_empty_dm_talk
+  
   belongs_to :dm_talk
   belongs_to :user
   validates :user_id, uniqueness: { scope: :dm_talk_id }
+
+  private
+
+    def destroy_empty_dm_talk
+      dm_talk.destroy if dm_talk.reload.dm_memberships.empty?
+    end
 end
