c0a5a40046469f6434fc9630d02c650c76b37482
living8peaks <bach5353op@gmail.com>
2021-04-29 08:50:28 +0900

    Set up validation for dm

--- app/models/dm_menmbership.rb
+++ app/models/dm_menmbership.rb
@@ -1,4 +1,5 @@
 class DmMenmbership < ApplicationRecord
   belongs_to :dm_talk
   belongs_to :user
+  validates :user_id, uniqueness: { scope: :dm_talk_id }
 end
--- app/models/dm_message.rb
+++ app/models/dm_message.rb
@@ -1,4 +1,7 @@
 class DmMessage < ApplicationRecord
   belongs_to :dm_talk
   belongs_to :user
+  validates :dm_talk_id, presence: true
+  validates :user_id,    presence: true
+  validates :content,    presence: true, length: { maximum: 2000 }
 end
