77b605b6a496b18d84ef389635f662c2cd48bd19
living8peaks <bach5353op@gmail.com>
2021-05-03 16:51:36 +0900

    Removed enums for agricultural land that users care about

--- db/migrate/20210503073459_add_default_checked_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddDefaultCheckedToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :checked, :integer, default: 0
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_03_073459) do
+ActiveRecord::Schema.define(version: 2021_05_02_225340) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -192,7 +192,6 @@ ActiveRecord::Schema.define(version: 2021_05_03_073459) do
     t.datetime "activated_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
-    t.integer "checked", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
